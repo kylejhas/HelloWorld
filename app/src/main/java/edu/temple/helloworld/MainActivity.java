3f552040eb7d0fb894385778ec5fb60680f33e31
Kyle Hastings <kylejhastings@gmail.com>
2019-09-17 23:32:44 -0400

    Added a horizontally centered button and added the ability to change textView from "Hello world. This is my first app!" to "The button has been clicked"

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        changeAfterClick();
+    }
+    //After the button is clicked, the old textView gets changed
+    private void changeAfterClick(){
+        final TextView afterClick = findViewById(R.id.textView);
+        Button myButton = findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                afterClick.setText("The button has been clicked");
+            }
+        });
     }
 }
